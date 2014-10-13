5ef9e065d2226036198d2ab6236b0976bbfffa73
Peter Kagey <peterkagey@gmail.com>
2014-10-12 23:23:17 -0700

    changed db to psql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141013055426) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: true do |t|
     t.integer  "vertices"
     t.integer  "level"
